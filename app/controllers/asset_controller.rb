302544a3334776907d90137c55af7539bf0ac2ad
Zenjutahi <zenjutahi@gmail.com>
2019-07-31 17:11:08 +0300

    [feature] - asset controller setup

--- app/controllers/asset_controller.rb
+++ app/controllers/asset_controller.rb
@@ -1,2 +1,43 @@
 class AssetController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @assets = current_user.assets
+  end
+  
+  def show
+    @asset = current_user.assets.find(asset_params[:id])
+  end
+  
+  def new
+    @asset = current_user.assets.new
+  end
+  
+  def create
+    @asset = current_user.assets.new(asset_params[:asset])
+    ...
+  end
+  
+  def edit
+    @asset = current_user.assets.find(asset_params[:id])
+  end
+  
+  def update
+    @asset = current_user.assets.find(asset_params[:id])
+  end
+  
+  def destroy
+    @asset = current_user.assets.find(asset_params[:id])
+    ...
+  end
+  
+  private
+  
+  def asset_params
+    # whitelist params
+    params.permit(:id, :asset)
+  end
+  # def set_assets
+  # end
+
 end
