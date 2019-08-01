f216fa32e889b193813a2a0757015f250b3720d7
Zenjutahi <zenjutahi@gmail.com>
2019-08-01 14:50:12 +0300

    [chore] - enable file upload and storage on app

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,12 +1,13 @@
 class AssetsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_asset, only: [:show, :destroy, :update,]
+   
   def index
     @assets = current_user.assets
   end
   
   def show
-    @asset = current_user.assets.find(asset_params[:id])
+    @asset
   end
   
   def new
@@ -14,30 +15,44 @@ class AssetsController < ApplicationController
   end
   
   def create
-    @asset = current_user.assets.new(asset_params[:asset])
-  end
-  
-  def edit
-    @asset = current_user.assets.find(asset_params[:id])
+    @asset = current_user.assets.new(asset_params)
+    if @asset.save
+      flash[:notice] = "Successfully uploaded the file..."
+
+      redirect_to assets_path
+    else
+      flash[:alert] = "Something went wrong ..."
+      redirect_to new_asset_path
+    end
   end
   
   def update
-    @asset = current_user.assets.find(asset_params[:id])
+    if @asset.update_attributes(asset_params)
+      flash[:notice] = "Succesfully edited!"
+      redirect_to assets_path
+    else
+      flash[:alert] = "Something went wrong..."
+      redirect_to assets_path
+    end
   end
   
   def destroy
-    @asset = current_user.assets.find(asset_params[:id])
+    if @asset.destroy
+      flash[:notice] = "Succesfully deleted!"
+    else
+      flash[:alert] = "Something went wrong..."
+    end
+    redirect_to assets_path
   end
   
   private
   
   def asset_params
     # whitelist params
-    params.permit(:id, :asset, :user_id, :uploaded_file)
+    params.require(:asset).permit([:uploaded_file])
   end
   
-  # def set_assets
-  #   @asset = 
-  # end
-
+  def set_asset
+    @asset = current_user.assets.find_by(id: params[:id])
+  end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -7,4 +7,13 @@ class Asset < ApplicationRecord
   
   validates_attachment_size :uploaded_file, :less_than => 10.megabytes
   validates_attachment_presence :uploaded_file
+  # validates :record_fingerprint, uniqueness: { message: 'Duplicate document.' }
+  
+  do_not_validate_attachment_file_type :uploaded_file
+  
+  def file_name
+    uploaded_file_file_name
+  end
+  
 end
+
