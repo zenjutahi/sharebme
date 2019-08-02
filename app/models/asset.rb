e7d2f3745c2735c6c87ec965bfe78afe3c723c0c
Zenjutahi <zenjutahi@gmail.com>
2019-08-02 17:05:24 +0300

    [feature] - enable storage on aws

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,8 +4,16 @@ class Asset < ApplicationRecord
   
   # Set up "uploaded_file" field as attached_file (using Paperclip)
   has_attached_file :uploaded_file,
-                  :url => "/assets/get/:id",
-                  :path => ":rails_root/storage/asset/:id/:basename.:extension"
+                  path: ":Rails_root/assets/:id/:basename.:extension",
+                  storage: :s3,
+                  s3_host_name: "sharebme.s3-eu-west-3.amazonaws.com",
+                  s3_credentials: {
+                    access_key_id: "AKIAXSDKOY47SLUY6Q7O",
+                    secret_access_key: "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",
+                    s3_region: "eu-west-3",
+                    bucket: "sharebme"          
+                  }
+                  
   
   validates_attachment_size :uploaded_file, :less_than => 10.megabytes
   validates_attachment_presence :uploaded_file
