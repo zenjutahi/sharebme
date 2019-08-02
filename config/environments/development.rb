127cdfe7a3d67425658e6051ac4cd6a6841c1ce3
Zenjutahi <zenjutahi@gmail.com>
2019-08-02 17:06:18 +0300

    [feature] - enable storage on aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,17 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
