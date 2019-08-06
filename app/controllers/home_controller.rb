0e2ed189f5cc3d8d8137c66fa5a02aa1987e75f9
Zenjutahi <zenjutahi@gmail.com>
2019-08-06 12:53:13 +0300

    [feature] - setting up the folder/file view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   
   def index
+    def index 
+    if user_signed_in? 
+      #load current_user's folders 
+      @folders = current_user.folders.order("name desc")   
     
+      #load current_user's files(assets) 
+      @assets = current_user.assets.order("uploaded_file_file_name desc")       
+    end
+end
   end
 end
