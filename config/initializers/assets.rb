50ae2bfbd089db854290aa7640fed7365ecdd1d7
Zenjutahi <zenjutahi@gmail.com>
2019-08-15 15:09:58 +0300

    [feature] -enable folder sharing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
+Rails.application.config.assets.precompile += %w( redmond/jquery-ui.css )
+Rails.application.config.assets.precompile += %w( redmond/jquery-ui.min.css )
+Rails.application.config.assets.precompile += %w( redmond/theme.css )
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
