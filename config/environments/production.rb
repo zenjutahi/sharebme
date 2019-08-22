da6c7411e02b6cb689d217759264d95d69638558
Zenjutahi <zenjutahi@gmail.com>
2019-08-22 15:05:02 +0300

    [chore] - cleanup uploads and sharing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://localhost:3000/" }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
