9772edb3d2e1539aaf3d62c64202083d0fe0ba34
Zenjutahi <zenjutahi@gmail.com>
2019-08-22 15:04:26 +0300

    [chore] - cleanup uploads and sharing

--- app/models/shared_folder.rb
+++ app/models/shared_folder.rb
@@ -1,6 +1,6 @@
 class SharedFolder < ApplicationRecord
   # this is for the owner(creator) of the assets 
-  belongs_to :user
+  belongs_to :user, optional: true
   # this is for the user to whom the owner has shared folders to
   belongs_to :shared_user, :class_name => "User", :foreign_key => "shared_user_id"
   # for the folder being shared
